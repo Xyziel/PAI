8d983d8feb4786bd6debd785eef1c1360445cc9e
Radosław Bujak <radobujak@wp.pl>
2020-01-19 11:20:32 +0100

    Add load profile

--- Controllers/BoardController.php
+++ Controllers/BoardController.php
@@ -18,12 +18,24 @@ class BoardController extends AppController {
 
     public function loadProfile() {
 
-        if ($this->isPost()) {
+        if (!$this->isPost() and isset($_SESSION['id'])) {
 
-        }
-        else if (isset($_SESSION['id'])) {
+            if (!isset($_SESSION['fileError'])) {
+                $_SESSION['fileError'] = null;
+            }
 
-            $this->renderPage('profile', ['name' => $_SESSION['user']->getName(), 'surname' => $_SESSION['user']->getSurname()]);
+            $userRepository = new UserRepository();
+            $user = $userRepository->getUser($_SESSION['id']);
+            $this->renderPage('profile', ['name' => $user->getName(),
+                                                'surname' => $user->getSurname(),
+                                                'age' => $user->getAge(),
+                                                'leg' => $user->getLeg(),
+                                                'club' => $user->getClub(),
+                                                'description' => $user->getDescription(),
+                                                'photo' => $user->getPhoto(),
+                                                'fileError' => $_SESSION['fileError']]);
+            $_SESSION['fileError'] = null;
+
         }
         else {
             $this->renderPage('login');
